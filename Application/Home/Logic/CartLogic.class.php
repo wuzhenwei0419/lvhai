c3b51e968f3e75391ea725038c5799ffb70eb784
wuzhenwei <wu.zhenwei@sohu.com>
2019-01-21 20:48:37 +0800

    已成功,我们将竭诚为您服务

--- Application/Home/Logic/CartLogic.class.php
+++ Application/Home/Logic/CartLogic.class.php
@@ -450,7 +450,7 @@ function cart_freight2($shipping_code,$province,$city,$district,$weight)
         {
             $wx_user = M('wx_user')->find();
             $jssdk = new \Mobile\Logic\Jssdk($wx_user['appid'],$wx_user['appsecret']);
-            $wx_content = "你刚刚下了一笔订单:{$order['order_sn']} 尽快支付,过期失效!";
+            $wx_content = "你刚刚下了一笔订单:{$order['order_sn']} 已成功,我们将竭诚为您服务!";
             $jssdk->push_msg($user['openid'],$wx_content);
         }
         return array('status'=>1,'msg'=>'提交订单成功','result'=>$order_id); // 返回新增的订单id        
